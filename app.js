6617de03b7c7c4fb2562ce8af6938a2733f34983
Nicholas Bernhard <nicholas@NDHFilms.com>
2018-06-26 13:08:40 -0600

    Additional comments

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
+$(document).ready(function() {
+
 //This app will generate GIFs based on a set of buttons on the web page.
 
 //This array contains a set of topics, from which HTML buttons will be made
 var topics = ["heavy metal", "math", "astronomy", "shakespeare", "stephen king"];
 
 function generateButtons() {
-
+    $("#buttonDiv").html("");
     for (i = 0; i <= topics.length - 1; i++) {
-        $("#buttonDiv").append("<button name='gifButton' value='" + topics[i] + "'>" + topics[i] + "</button>");
+        $("#buttonDiv").append("<button name='gifButton' class='buttonRow' value='" + topics[i] + "'>" + topics[i] + "</button>");
     }
 
 };
+
+$("#submitButton").on("click", function() {
+    console.log("Submit button clicked!");
+
+    topics.push($("#textEntry").val());
+  
+    $("#buttonDiv").append("<button name='gifButton' class='buttonRow' value='" + topics[topics.length - 1] + "'>" + topics[topics.length - 1] + "</button>");
+
+});
 
 //A button's value will be assigned to the variable q upon clicking it. Here, q is declared as a global variable.
 var q;
 
 generateButtons();
 
-//Key for the Giphy API:
-var APIKey = "B27Fi15mjSRFQC15ZFdFcvK5Xkenf29B";
-
-$("button").on("click", function() {
+//Exclude the Submit button from calling the GIF function
+$(".buttonRow").on("click", function() {
+    //Clearing the container makes it run a little smoother, it seems
+    $("#gifDivContainer").html("");
     //Assigning the value of the clicked button to the q variable
     q = $(this).val();
+    console.log(q);
+
+    //Key for the Giphy API:
+    var APIKey = "B27Fi15mjSRFQC15ZFdFcvK5Xkenf29B";
 
     //Declaring the Giphy API's query URL as a variable, incorporating our API key and search term
     //The "&limit5" will produce 5 results per click
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + q + "&api_key=" + APIKey + "&limit=5";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + q + "&api_key=" + APIKey + "&limit=10";
 
     $.ajax({
       url: queryURL,
@@ -45,7 +60,10 @@ $("button").on("click", function() {
         //Create an img tag for the gif
         var personImage = $("<img>");
         //Assign the GIF's url to the img tag's src attribute
-        personImage.attr("src", results[i].images.fixed_height.url);
+        personImage.attr("data-still", results[i].images.fixed_height_still.url);
+        personImage.attr("data-animate", results[i].images.fixed_height.url);
+        personImage.attr("src", personImage.attr("data-still"));
+        personImage.attr("data-state", "still");
         //Add the paragraph and img tags to the GIF div
         gifDiv.prepend(p);
         gifDiv.prepend(personImage);
@@ -53,34 +71,28 @@ $("button").on("click", function() {
         $(".gifDivContainer").prepend(gifDiv);
 
         }
+
+        $("img").on("click", function() {
+        
+            console.log("clicked");
+        
+            var state = $(this).attr("data-state");
+        
+            if (state === "still") {
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+            }
+        
+             if (state === "animate") {
+              $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+        
+            }
+        
+        });
+
       });
+
   });
 
-// $(".gif").on("click", function() {
-//     // STEP ONE: study the html above.
-//     // Look at all the data attributes.
-//     // Run the file in the browser. Look at the images.
-
-//     // After we complete steps 1 and 2 we'll be able to pause gifs from giphy.
-
-//     // STEP TWO: make a variable named state and then store the image's data-state into it.
-//     // Use the .attr() method for this.
-
-//     // ============== FILL IN CODE HERE FOR STEP TWO =========================
-
-//     // CODE GOES HERE
-
-//     var state = $(this).attr("data-state");
-
-//     if (state === "still") {
-//       $(this).attr("src", $(this).attr("data-animate"));
-//       $(this).attr("data-state", "animate");
-//     }
-
-//      if (state === "animate") {
-//       $(this).attr("src", $(this).attr("data-still"));
-//       $(this).attr("data-state", "still");
-
-//     }
-
-// });
+});
