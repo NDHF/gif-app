7b543998d54d79e18356e8e594a7c27289f1b835
Nicholas Bernhard <nicholas@NDHFilms.com>
2018-06-25 21:46:06 -0600

    Basic functionality works

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var topics = ["heavy metal", "math", "astronomy", "shakespeare", "stephen king"];
+//This app will generate GIFs based on a set of buttons on the web page.
 
-console.log(topics.length);
+//This array contains a set of topics, from which HTML buttons will be made
+var topics = ["heavy metal", "math", "astronomy", "shakespeare", "stephen king"];
 
 function generateButtons() {
 
@@ -10,29 +11,20 @@ function generateButtons() {
 
 };
 
+//A button's value will be assigned to the variable q upon clicking it. Here, q is declared as a global variable.
 var q;
 
 generateButtons();
 
+//Key for the Giphy API:
 var APIKey = "B27Fi15mjSRFQC15ZFdFcvK5Xkenf29B";
 
-//javascript, jQuery
-
-// $("button").on("click", function() {
-
-// q = $(this).val();
-// console.log(q);
-
-// var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + q + "&api_key=" + APIKey + "&limit=5");
-// xhr.done(function(data) { console.log("success got data", data); });
-
-// $("#gifDiv").append(data[0].images.fixed_height.url);
-
-// });
-
 $("button").on("click", function() {
+    //Assigning the value of the clicked button to the q variable
     q = $(this).val();
-    console.log(q);
+
+    //Declaring the Giphy API's query URL as a variable, incorporating our API key and search term
+    //The "&limit5" will produce 5 results per click
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + q + "&api_key=" + APIKey + "&limit=5";
 
     $.ajax({
@@ -42,24 +34,24 @@ $("button").on("click", function() {
       .then(function(response) {
         var results = response.data;
 
-        console.log(results);
-        console.log(results.data);
-
+        //Create a div, with a GIF inside, for each of the results
         for (i = 0; i < results.length; i++) {
-          var gifDiv = $("<div class='item'>");
+        //Div to hold the GIF
+        var gifDiv = $("<div class='item'>");
+        //Get the age-appropriateness rating of the GIF
+        var rating = results[i].rating;
+        //Add rating to a paragraph
+        var p = $("<p>").text("Rating: " + rating);
+        //Create an img tag for the gif
+        var personImage = $("<img>");
+        //Assign the GIF's url to the img tag's src attribute
+        personImage.attr("src", results[i].images.fixed_height.url);
+        //Add the paragraph and img tags to the GIF div
+        gifDiv.prepend(p);
+        gifDiv.prepend(personImage);
+        //Add the GIF div to the container
+        $(".gifDivContainer").prepend(gifDiv);
 
-          var rating = results[i].rating;
-          console.log(rating);
-
-          var p = $("<p>").text("Rating: " + rating);
-
-          var personImage = $("<img>");
-          personImage.attr("src", results[i].images.fixed_height.url);
-
-          gifDiv.prepend(p);
-          gifDiv.prepend(personImage);
-
-          $(".gifDivContainer").prepend(gifDiv);
         }
       });
   });
